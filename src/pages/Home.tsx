06cfa5185a2a69703a83a30f9e0e49fd5764e59f
João Victor <jmvic26pc@gmail.com>
2026-02-13 22:57:35 -0300

    Introdução á pagina inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,8 @@
 function Home() {
   return (
-    <div className="h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-600">
-        NestPhone + Tailwind!
-      </h1>
+    <div className="w-full h-screen bg-bg flex flex-col p-4">
+        <h1 className="font-title text-title text-6xl mx-auto pt-4 pb-3">TechPhone</h1>
+        <h3 className="text-white font-text text-2xl max-w-[70%] mx-auto text-center p-2">O NestPhone é uma jornada imersiva pela história da mobilidade humana. De 1973 aos dias atuais, exploramos como o celular deixou de ser um luxo pesado para se tornar uma extensão da nossa inteligência. Navegue por décadas de inovação, descubra os designs que definiram eras e entenda a tecnologia que moldou o futuro</h3>
     </div>
   )
 }
